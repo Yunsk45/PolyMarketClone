ac83b69217a56c117f1af18fbd8346341ba13260
Farlonx <nisrecht@gmail.com>
2022-03-01 11:18:15 +0100

    Parameters for Polymarket contract

--- contracts/Polymarket.sol
+++ contracts/Polymarket.sol
@@ -12,4 +12,46 @@ contract Polymarket {
       owner = msg.sender;
       polyToken = _polyToken;
    }
+
+   mapping(uint256 => Markets) public markets;
+
+   struct Markets {
+      uint256 id;
+      string market;
+      uint256 timestamp;
+      uint256 endTimestamp;
+      address createdBy;
+      string creatorImageHash;
+      AmoundAdded[] yesCount;
+      AmoundAdded[] noCount;
+      uint256 totalAmount;
+      uint256 totalYesAmount;
+      uint256 totalNoAmount;
+      bool eventCompleted;
+      string description;
+      string resolverUrl;
+   }
+
+   struct AmountAdded {
+      address user;
+      uint256 amount;
+      uint256 timestamp;
+   }
+
+   mapping(address => uint256) public winningAmount;
+   address[] public winningAddresses;
+
+   event MarketCreated(
+      uint256 id,
+      string market,
+      uint256 timestamp,
+      address createdBy,
+      string creatorImageHash,
+      uint256 totalAmount,
+      uint256 totalYesAmount,
+      uint256 totalNoAmount
+   );
+
+
+
 }
