e984677a28980eb603c421e285063e949a76607b
Farlonx <nisrecht@gmail.com>
2022-03-01 12:01:49 +0100

    Migrations to deploy PolyToken and Polymarket

---
+++ migrations/2_polytoken_polymarket.js
@@ -0,0 +1,9 @@
+const PolyToken = artifacts.require("PolyToken");
+const Polymarket = artifacts.require("Polymarket");
+
+module.exports = async function (deployer) {
+  await deployer.deploy(PolyToken);
+  const polyToken = await PolyToken.deployed();
+
+  await deployer.deploy(Polymarket, polyToken.address)
+}
